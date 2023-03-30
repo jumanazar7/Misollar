a1210084f9b6e877079515407ec7f386d9704b5b
jumanazar7 <qodirberganovjumanazar186@gmail.com>
2023-03-30 11:31:57 +0500

    jadval

---
+++ jadval.py
@@ -0,0 +1,15 @@
+x1k1 = float(input("X1K1: "))
+x1k2 = float(input("X1K2: "))
+x1k3 = float(input("X1K3: "))
+x1k4 = float(input("X1K4: "))
+x1k5 = float(input("X1K5: "))
+
+x2k1 = float(input("X2K1: "))
+x2k2 = float(input("X2K2: "))
+x2k3 = float(input("X2K3: "))
+x2k4 = float(input("X2K4: "))
+x2k5 = float(input("X2K5: "))
+
+mn = min(1-x1k1,x2k1),min(1-x1k2,x2k2),min(1-x1k3,x2k3),min(1-x1k4,x2k4),min(1-x1k4,x2k4)
+print(max(mn))
+
